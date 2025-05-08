94957dc8fe3de65f8dbfc150021324a593a0b16f
osman-tkdmr <osman2001.tekdamar@gmail.com>
2025-05-08 17:30:11 +0300

    the end fixing repo

--- Notebooks/path_config.py
+++ Notebooks/path_config.py
@@ -1,7 +1,42 @@
-dl_data = "../Data/5G KONUMLANDIRMA YARISMA VERISI/5G Saha Ölçüm Verileri (GPS mevcut)/5G_DL.xlsx"
-ul_data = "../Data/5G KONUMLANDIRMA YARISMA VERISI/5G Saha Ölçüm Verileri (GPS mevcut)/5G_UL.xlsx"
-Scanner_data = "../Data/5G KONUMLANDIRMA YARISMA VERISI/5G Saha Ölçüm Verileri (GPS mevcut)/5G_Scanner.xlsx"
-bs_data = "../Data/5G KONUMLANDIRMA YARISMA VERISI/5G Baz İstasyonu Konfigürasyon Verileri/İTÜ 5G Hücre Bilgileri.xlsx"
-train_data = "../Data/5G KONUMLANDIRMA YARISMA VERISI/5G Saha Ölçüm Verileri (GPS mevcut)/y_train.csv"
-test_data = "../Data/5G KONUMLANDIRMA YARISMA VERISI/5G Saha Ölçüm Verileri (GPS mevcut)/y_test.csv"
-predict_data = "../Data/5G KONUMLANDIRMA YARISMA VERISI/5G Saha Ölçüm Verileri (GPS mevcut)/predictions.csv"
+from pathlib import Path
+import os
+
+class PathConfig:
+    def __init__(self, base_dir= None):
+        if base_dir is None:
+            self._base_dir = Path("..").resolve()
+        else:
+            self._base_dir = Path(base_dir).resolve()
+        
+        self._data_dir = self._base_dir / "Data"
+        self._raw_dir = self._data_dir / "Raw"
+        self._processed_dir = self._data_dir / "Processed"
+
+        self._raw_konfigurasyon_dir = self._data_dir / self._raw_dir / "5G Baz İstasyonu Konfigürasyon Verileri"
+        self._raw_saha_olcum_dir = self._data_dir / self._raw_dir / "5G Saha Ölçüm Verileri (GPS mevcut)"
+        self._raw_harita_dir = self._data_dir / self._raw_dir / "İTÜ Kampüs Harita Verileri"
+
+        self._processed_konfigurasyon_dir = self._data_dir / self._processed_dir / "5G Baz İstasyonu Konfigürasyon Verileri"
+        self._processed_saha_olcum_dir = self._data_dir / self._processed_dir / "5G Saha Ölçüm Verileri (GPS mevcut)"
+        self._processed_harita_dir = self._data_dir / self._processed_dir / "İTÜ Kampüs Harita Verileri"
+        self._processed_other_dir = self._data_dir / self._processed_dir / "Other"
+        ##########3
+
+        self.Hucre_Bilgileri = self._raw_konfigurasyon_dir / "İTÜ 5G Hücre Bilgileri.xlsx"
+
+        self.dl_path = self._raw_saha_olcum_dir / "5G_DL.xlsx"
+        self.ul_path = self._raw_saha_olcum_dir / "5G_UL.xlsx"
+        self.scanner_path = self._raw_saha_olcum_dir / "5G_Scanner.xlsx"
+
+
+        self.build_path = self._raw_harita_dir / "ITU_3DBINA_EPSG4326.shp"
+        self.vege_path = self._raw_harita_dir / "ITU_3DVEGETATION_EPSG4326.shp"
+        self.wathers_path = self._raw_harita_dir / "ITU_SUKUTLESI_EPSG4326.shp"
+        self.roads_path = self._raw_harita_dir / "ITU_ULASIMAGI_EPSG4326.shp"
+        # sadece şu ana kadar kullandığımız harita dosylarını koydum,
+        # diğer harita dosyaları için de aynı şekilde path tanımlayabilirsin.
+
+        self.parquet_data = self._processed_saha_olcum_dir / "5G_DataSet.parquet"
+        self.train_df_path = self._processed_saha_olcum_dir / "y_train.csv"
+        self.test_df_path = self._processed_saha_olcum_dir / "y_test.csv"
+        self.predictions_df_path = self._processed_saha_olcum_dir / "predictions.csv"
